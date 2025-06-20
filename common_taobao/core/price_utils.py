d41d089360cfde4b6c39275ce0dab060b24f3a1b
angelaxu76 <67590618+angelaxu76@users.noreply.github.com>
2025-06-21 06:49:48 +0800

    clarks全部工作正常

--- common_taobao/core/price_utils.py
+++ common_taobao/core/price_utils.py
@@ -8,9 +8,14 @@ def calculate_discount_price(info: dict) -> float:
         base_price = float(info.get("AdjustedPrice") or info.get("Price") or 0)
         custom_rate = 1.2
         delivery_cost = 12
-        profit = 1.2
         discount_Space = 1.15
         exchange_rate = 9.8
+
+        if base_price > 50:
+            profit = 1.07
+        else:
+            profit = 1.25
+
         rmb_price = (base_price * custom_rate + delivery_cost) * profit * discount_Space * exchange_rate
         rounded_price = int(round(rmb_price / 10.0)) * 10
         return rounded_price
