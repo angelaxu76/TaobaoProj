82858d6cbc2c01ebbe68eeef9f90043a26b21884
maddingzhou <xunianzhou1@gmail.com>
2025-12-03 01:04:53 +0000

    ABC

--- brands/barbour/pipeline/fetch_info_pipeline.py
+++ brands/barbour/pipeline/fetch_info_pipeline.py
@@ -16,7 +16,7 @@ from brands.barbour.supplier.terraces_get_links import collect_terraces_links
 from brands.barbour.supplier.philipmorrisdirect_get_links import philipmorris_get_links
 from brands.barbour.supplier.cho_get_links import cho_get_links
 from brands.barbour.supplier.cho_fetch_info import cho_fetch_info
-from brands.barbour.legacy.philipmorrisdirect_fetch_info import philipmorris_fetch_info_v3
+from brands.barbour.legacy.philipmorrisdirect_fetch_info import philipmorris_fetch_info
 from brands.barbour.common.barbour_import_to_barbour_products import batch_import_txt_to_barbour_product
 from brands.barbour.common.import_supplier_to_db_offers import import_txt_for_supplier
 from brands.barbour.jingya.insert_jingyaid_mapping import insert_jingyaid_to_db,clear_barbour_inventory,insert_missing_products_with_zero_stock
@@ -36,17 +36,17 @@ def barbour_database_import_pipleline():
     # very_get_links()
     # collect_terraces_links()
     # philipmorris_get_links()
-    cho_get_links()
+    # cho_get_links()
 
     print("步骤 2：抓取商品信息并存为TXT")
-    # barbour_fetch_info()
+    barbour_fetch_info()
     # outdoorandcountry_fetch_info(max_workers=15)
-    allweathers_fetch_info(7)
+    # allweathers_fetch_info(7)
     # houseoffraser_fetch_info(max_workers=15, headless=False)
     # very_fetch_info(max_workers=15)
     # terraces_fetch_info(max_workers=15)
-    philipmorris_fetch_info_v3(max_workers=15)
-    cho_fetch_info(max_workers=15)
+    # philipmorris_fetch_info(max_workers=15)
+    # cho_fetch_info(max_workers=15)
 
 
     
