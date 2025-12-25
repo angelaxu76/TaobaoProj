c4a35d6ae43f70eaf6c54816ca7d9fd76007615a
angelaxu76 <67590618+angelaxu76@users.noreply.github.com>
2025-12-25 01:55:34 +0000

    ABC

--- helper/image/group_images.py
+++ helper/image/group_images.py
@@ -3,13 +3,13 @@ import shutil
 from math import ceil
 
 # 原始图片目录
-source_dir = r"C:\Users\martin\Desktop\image_process"
+source_dir = r"D:\TB\Products\clarks_jingya\publication\image_cutter"
 
 # 目标根目录（可以修改为其他路径）
-target_root_dir = r"C:\Users\martin\Desktop\image_process分组图片"
+target_root_dir = r"D:\TB\Products\clarks_jingya\publication\image_cutter分组图片"
 
 # 每组的图片数量
-group_size = 320
+group_size = 350
 
 # 支持的图片扩展名
 image_extensions = ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.webp']
