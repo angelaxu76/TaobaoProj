68996763f59a218325f36a7111191ad31a3a6868
madding <angela112621@gmail.com>
2026-02-19 18:44:32 +0000

    change camper、 prices

--- brands/barbour/pipeline/image_html_process.py
+++ brands/barbour/pipeline/image_html_process.py
@@ -10,56 +10,61 @@ from helper.image.trim_sides_batch import trim_sides_batch
 import os
 
 def main():
-    code_file_path = r"D:\TB\Products\barbour\repulibcation\publication_codes.txt"
+    code_file_path = r"D:\TB\Products\barbour\repulibcation\codes.txt"
 
 
-    excel_path = r"D:\TB\Products\barbour\document\publication\barbour_publication_20260215_144753.xlsx"
-    downloaded_dir = r"D:\TB\Products\barbour\images_download"
-    processed_dir = r"D:\TB\Products\barbour\images"
-    publish_ready_dir = r"D:\TB\Products\barbour\repulibcation\images_selected"
-    publish_need_process_dir = r"D:\TB\Products\barbour\repulibcation\need_edit"
-    missing_txt_path = r"D:\TB\Products\barbour\repulibcation\missing_codes.txt"
+    # excel_path = r"D:\TB\Products\barbour\document\publication\barbour_publication_20260215_144753.xlsx"
+    # downloaded_dir = r"D:\TB\Products\barbour\images_download"
+    # processed_dir = r"D:\TB\Products\barbour\images"
+    # publish_ready_dir = r"D:\TB\Products\barbour\repulibcation\images_selected"
+    # publish_need_process_dir = r"D:\TB\Products\barbour\repulibcation\need_edit"
+    # missing_txt_path = r"D:\TB\Products\barbour\repulibcation\missing_codes.txt"
 
-    ready, need_edit, missing = prepare_images_for_publication(
-        excel_path=excel_path,
-        downloaded_dir=downloaded_dir,
-        processed_dir=processed_dir,
-        publish_ready_dir=publish_ready_dir,
-        publish_need_process_dir=publish_need_process_dir,
-        missing_txt_path=missing_txt_path,
-        verbose=True,
-    )
-    
-    # rebuild_all_products_images(target_root, verbose=True)
-
-    # collect_all_images_to_flat_dir(
-    #     target_root,
-    #     flat_images_dir,
+    # ready, need_edit, missing = prepare_images_for_publication(
+    #     excel_path=excel_path,
+    #     downloaded_dir=downloaded_dir,
+    #     processed_dir=processed_dir,
+    #     publish_ready_dir=publish_ready_dir,
+    #     publish_need_process_dir=publish_need_process_dir,
+    #     missing_txt_path=missing_txt_path,
     #     verbose=True,
     # )
-
-    # watermark_index_0_9_inplace(
-    # flat_images_dir,
-    # watermark_text="英国哈梅尔百货",
-    # )
+    
 
 
+    flat_images_dir=BARBOUR["IMAGE_PROCESS"]
+    target_root = r"D:\TB\Products\barbour\repulibcation\images_selected"
 
-    # print("将图片merge到一张图片中")
-    # batch_merge_images(BARBOUR["IMAGE_PROCESS"],BARBOUR["MERGED_DIR"], width=750)
+    rebuild_all_products_images(target_root, verbose=True)
 
-    # print("生成产品详情卡HTML")
-    # generate_html_from_codes_files("barbour",code_file_path,max_workers=2)
-    # generate_first_page_from_codes_files("barbour",code_file_path)
+    collect_all_images_to_flat_dir(
+        target_root,
+        flat_images_dir,
+        verbose=True,
+    )
 
-    # # GECKODRIVER_PATH = r"D:\Software\geckodriver.exe"  # GeckoDriver 路径
-    # print("生成产品详情卡图片")
-    # convert_html_to_images(BARBOUR["HTML_DIR_DES"], BARBOUR["HTML_IMAGE_DES"],"",6)
-    # trim_sides_batch(BARBOUR["HTML_IMAGE_DES"],BARBOUR["HTML_CUTTER_DES"])
+    watermark_index_0_9_inplace(
+    flat_images_dir,
+    watermark_text="英国哈梅尔百货",
+    )
 
-    # print("生成产品首页图片")
-    # convert_html_to_images(BARBOUR["HTML_DIR_FIRST_PAGE"], BARBOUR["HTML_IMAGE_FIRST_PAGE"],"",6)
-    # trim_sides_batch(BARBOUR["HTML_IMAGE_FIRST_PAGE"],BARBOUR["HTML_CUTTER_FIRST_PAGE"])
+
+
+    print("将图片merge到一张图片中")
+    batch_merge_images(BARBOUR["IMAGE_PROCESS"],BARBOUR["MERGED_DIR"], width=750)
+
+    print("生成产品详情卡HTML")
+    generate_html_from_codes_files("barbour",code_file_path,max_workers=2)
+    generate_first_page_from_codes_files("barbour",code_file_path)
+
+    # GECKODRIVER_PATH = r"D:\Software\geckodriver.exe"  # GeckoDriver 路径
+    print("生成产品详情卡图片")
+    convert_html_to_images(BARBOUR["HTML_DIR_DES"], BARBOUR["HTML_IMAGE_DES"],"",6)
+    trim_sides_batch(BARBOUR["HTML_IMAGE_DES"],BARBOUR["HTML_CUTTER_DES"])
+
+    print("生成产品首页图片")
+    convert_html_to_images(BARBOUR["HTML_DIR_FIRST_PAGE"], BARBOUR["HTML_IMAGE_FIRST_PAGE"],"",6)
+    trim_sides_batch(BARBOUR["HTML_IMAGE_FIRST_PAGE"],BARBOUR["HTML_CUTTER_FIRST_PAGE"])
 
 
 
