2cc26036cc12c978a5b4ae73131986f7ed9b6ee9
madding <angela112621@gmail.com>
2026-02-19 18:48:35 +0000

    new db

--- cfg/db_config.py
+++ cfg/db_config.py
@@ -1,17 +1,17 @@
 # config/db_config.py
-PGSQL_CONFIG = {
-    "host": "192.168.1.44",
-    "port": 5432,
-    "user": "postgres",
-    "password": "516518",
-    "dbname": "taobao_inventory_db",
-}
-
 # PGSQL_CONFIG = {
 #     "host": "192.168.1.44",
 #     "port": 5432,
 #     "user": "postgres",
 #     "password": "516518",
-#     "dbname": "eminzora_inventory_db",
+#     "dbname": "taobao_inventory_db",
 # }
+
+PGSQL_CONFIG = {
+    "host": "192.168.1.44",
+    "port": 5432,
+    "user": "postgres",
+    "password": "516518",
+    "dbname": "eminzora_inventory_db",
+}
 
