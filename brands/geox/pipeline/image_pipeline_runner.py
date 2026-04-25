7a5eac9dca964fcdb04560a434b1047f26ce5bad
madding <angela112621@gmail.com>
2026-04-25 22:33:39 +0100

    image

--- brands/geox/pipeline/image_pipeline_runner.py
+++ brands/geox/pipeline/image_pipeline_runner.py
@@ -17,22 +17,22 @@ def main():
 
 
     print("下载指定商品编码的的图片")
-    download_geox_images_by_code_file(code_file_path)
+    # download_geox_images_by_code_file(code_file_path)
 
     # print("下载所有商品图片")
     # download_all_images_from_product_links()
 
-    print("图抖动加上水平翻转")
-    INPUT_DIR = Path(GEOX["IMAGE_DOWNLOAD"])
-    OUTPUT_DIR = Path(GEOX["IMAGE_PROCESS"])
-    OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
-    batch_process_images(INPUT_DIR,OUTPUT_DIR)
+    # print("图抖动加上水平翻转")
+    # INPUT_DIR = Path(GEOX["IMAGE_DOWNLOAD"])
+    # OUTPUT_DIR = Path(GEOX["IMAGE_PROCESS"])
+    # OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
+    # batch_process_images(INPUT_DIR,OUTPUT_DIR)
 
-    print("最大化灰度裁剪图片")
-    bg_color = (240, 240, 240)
-    tolerance = 35
-    quality = 85
-    run_crop_and_expand(GEOX["IMAGE_PROCESS"], GEOX["IMAGE_CUTTER"], bg_color, tolerance, quality)
+    # print("最大化灰度裁剪图片")
+    # bg_color = (240, 240, 240)
+    # tolerance = 35
+    # quality = 85
+    # run_crop_and_expand(GEOX["IMAGE_PROCESS"], GEOX["IMAGE_CUTTER"], bg_color, tolerance, quality)
 
     print("抠图并转换为白底图（加水印）")
     _cutmod.AUTO_CUTOUT   = True
@@ -42,29 +42,30 @@ def main():
         str(GEOX["IMAGE_CUTTER"]),
         str(GEOX["IMAGE_CUTTER"]),
         max_workers=4,
+        add_watermark=False
     )
 
-    print("将处理好的图片copy到document目录")
-    copy_images(GEOX["IMAGE_CUTTER"],GEOX["IMAGE_DIR"])
+    # print("将处理好的图片copy到document目录")
+    # copy_images(GEOX["IMAGE_CUTTER"],GEOX["IMAGE_DIR"])
 
 
-    print("将图片merge到一张图片中")
-    batch_merge_images(GEOX["IMAGE_CUTTER"],GEOX["MERGED_DIR"], width=750)
+    # print("将图片merge到一张图片中")
+    # batch_merge_images(GEOX["IMAGE_CUTTER"],GEOX["MERGED_DIR"], width=750)
 
 
 
-    print("生成产品详情卡HTML")
-    generate_html_from_codes_files("GEOX",code_file_path)
-    generate_first_page_from_codes_files("GEOX",code_file_path)
+    # print("生成产品详情卡HTML")
+    # generate_html_from_codes_files("GEOX",code_file_path)
+    # generate_first_page_from_codes_files("GEOX",code_file_path)
 
-    GECKODRIVER_PATH = r"D:\Software\geckodriver.exe"  # GeckoDriver 路径
-    print("生成产品详情卡图片")
-    convert_html_to_images(GEOX["HTML_DIR_DES"], GEOX["HTML_IMAGE_DES"],"",6)
-    trim_sides_batch(GEOX["HTML_IMAGE_DES"],GEOX["HTML_CUTTER_DES"])
+    # GECKODRIVER_PATH = r"D:\Software\geckodriver.exe"  # GeckoDriver 路径
+    # print("生成产品详情卡图片")
+    # convert_html_to_images(GEOX["HTML_DIR_DES"], GEOX["HTML_IMAGE_DES"],"",6)
+    # trim_sides_batch(GEOX["HTML_IMAGE_DES"],GEOX["HTML_CUTTER_DES"])
 
-    print("生成产品首页图片")
-    convert_html_to_images(GEOX["HTML_DIR_FIRST_PAGE"], GEOX["HTML_IMAGE_FIRST_PAGE"],"",6)
-    trim_sides_batch(GEOX["HTML_IMAGE_FIRST_PAGE"],GEOX["HTML_CUTTER_FIRST_PAGE"])
+    # print("生成产品首页图片")
+    # convert_html_to_images(GEOX["HTML_DIR_FIRST_PAGE"], GEOX["HTML_IMAGE_FIRST_PAGE"],"",6)
+    # trim_sides_batch(GEOX["HTML_IMAGE_FIRST_PAGE"],GEOX["HTML_CUTTER_FIRST_PAGE"])
 
 
 if __name__ == "__main__":
--- test/testPrice.py
+++ test/testPrice.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from common.pricing.price_utils import calculate_jingya_prices
 
 def main():
-    untaxed, retail = calculate_jingya_prices(160,7,9.7)
+    untaxed, retail = calculate_jingya_prices(73,7,9.3)
 
 
     print(f"📂 鲸芽价格: \n{untaxed}")
