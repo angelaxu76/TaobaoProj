7949061637689173241aa9d51fe9b1fe416ac00a
angelaxu76 <angela112621@gmail.com>
2026-09-09 18:33:58 +0100

    bbb

--- ops/shipping/run_send_ecms_notification.py
+++ ops/shipping/run_send_ecms_notification.py
@@ -121,8 +121,8 @@ def main():
     send_ecms_notification(
         shipment_ref = SHIPMENT_REF,
         couriers     = [
-            ("GI048705295GB", "Parcelforce"),
-            ("GI048705287GB", "Parcelforce"),
+            ("GI048980165GB", "Parcelforce"),
+            ("GI048980179GB", "Parcelforce"),
         ],
         lp_file      = r"D:\Projects\VS-TaobaoProj\TaobaoProj\ops\shipping\lp_numbers.txt",
         ship_date    = SHIP_DATE,
